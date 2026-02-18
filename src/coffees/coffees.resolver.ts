c0c2bf69e97fbaa59f59d0f1381523c9d156a7a3
tklasnakov <t.klasnakov@gmail.com>
2026-02-18 07:15:24 +0200

    create empty service for coffee crud operations

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -2,23 +2,24 @@ import { Resolver, Query, Args, ID, Mutation } from '@nestjs/graphql';
 import { Coffee } from '../coffee/entities/coffee.entity/coffee.entity';
 import { ParseIntPipe } from '@nestjs/common';
 import { CreateCoffeeInput } from './dto/create-coffee.input';
+import { CoffeesService } from './coffees.service';
 
 @Resolver()
 export class CoffeesResolver {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
   @Query(() => [Coffee], { name: 'coffees' })
-  async findAll() {
-    return [];
+  findAll() {
+    return this.coffeeService.findAll();
   }
 
   @Query(() => Coffee, { name: 'coffee', nullable: true })
-  async findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
-    return null;
+  findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
+    return this.coffeeService.findOne(id);
   }
 
   @Mutation(() => Coffee, { name: 'createCoffee', nullable: true })
-  async create(
-    @Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput,
-  ) {
-    return null;
+  create(@Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput) {
+    return this.coffeeService.create(createCoffeeInput);
   }
 }
